7831c0d8902735342a7f33995f03445df6fcc397
tulparstudyo <teknovipyazilim@gmail.com>
2024-08-15 22:43:03 +0300

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,6 +12,7 @@ class PageController extends Controller {
     public function index(Request $request, $pageSlug){
         $data['config_general']   =  [];
         if(\View::exists('page.'.$pageSlug)){
+            $data['page'] = Page::where(['permalink'=>$pageSlug])->first() ;
             return view('page.'.$pageSlug, $data);
         }else {
             $page = Page::where(['permalink'=>$pageSlug])->first();
